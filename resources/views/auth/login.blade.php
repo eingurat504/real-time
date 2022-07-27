4ebccc8777c8182212887de548c030d3e2638683
emma ingurat <e.ingurat@bsmart-technology.com>
2022-07-27 09:42:48 +0300

    login blade fixes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,53 @@
 @extends('layouts.app')
 
-@section('content')
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Kapella Bootstrap Admin Dashboard Template</title>
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-  <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" />
-</head>
-<body>
-  <div class="container-scroller">
-    <div class="container-fluid page-body-wrapper full-page-wrapper">
-      <div class="main-panel">
-        <div class="content-wrapper d-flex align-items-center auth px-0">
-          <div class="row w-100 mx-0">
-            <div class="col-lg-4 mx-auto">
-              <div class="auth-form-light text-left py-5 px-4 px-sm-5">
-                <div class="brand-logo">
-                  <img src="{{ asset('images/logo.svg') }}" alt="logo">
-                </div>
-                <h4>Hello! let's get started</h4>
-                <h6 class="font-weight-light">Sign in to continue.</h6>
-                <form class="pt-3" method="POST" action="{{ route('login') }}">
-                  <div class="form-group">
-                    <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Username">
-                    @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                  </div>
-                  <div class="form-group">
-                    <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password">
-                    @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                  </div>
-                  <div class="mt-3">
-                    <button type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">
-                        {{ __('SIGN IN') }}
-                    </button>
-                  </div>
-                  <div class="my-2 d-flex justify-content-between align-items-center">
-                    <div class="form-check">
-                      <label class="form-check-label text-muted">
-                        <input type="checkbox" class="form-check-input">
-                        Keep me signed in
-                      </label>
-                    </div>
-                    @if (Route::has('password.request'))
-                        <a class="auth-link text-black" href="{{ route('password.request') }}">
-                            {{ __('Forgot Password?') }}
-                        </a>
-                    @endif
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
+@section('main-content')
+<div class="content-wrapper d-flex align-items-center auth px-0">
+  <div class="row w-100 mx-0">
+    <div class="col-lg-4 mx-auto">
+      <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+        <div class="brand-logo">
+          <img src="{{ asset('images/logo.svg') }}" alt="logo">
         </div>
+        <h4>Hello! let's get started</h4>
+        <h6 class="font-weight-light">Sign in to continue.</h6>
+        <form class="pt-3" method="POST" action="{{ route('login') }}">
+          <div class="form-group">
+            <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Username">
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+          </div>
+          <div class="form-group">
+            <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password">
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+          </div>
+          <div class="mt-3">
+            <button type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">
+                {{ __('SIGN IN') }}
+            </button>
+          </div>
+          <div class="my-2 d-flex justify-content-between align-items-center">
+            <div class="form-check">
+              <label class="form-check-label text-muted">
+                <input type="checkbox" class="form-check-input">
+                Keep me signed in
+              </label>
+            </div>
+            @if (Route::has('password.request'))
+                <a class="auth-link text-black" href="{{ route('password.request') }}">
+                    {{ __('Forgot Password?') }}
+                </a>
+            @endif
+          </div>
+        </form>
       </div>
     </div>
   </div>
-</body>
-
-</html>
+</div>
 @endsection
