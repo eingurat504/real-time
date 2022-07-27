99dce562e3352475e3ab7243caa418a72c23d532
emma ingurat <e.ingurat@bsmart-technology.com>
2022-07-27 14:24:45 +0300

    index updates

--- resources/views/routes/index.blade.php
+++ resources/views/routes/index.blade.php
@@ -3,35 +3,37 @@
 @section('content')
 <div class="col-lg-12 grid-margin stretch-card">
     <div class="card">
-    <div class="card-body">
-        <h4 class="card-title">Routes</h4>
-        <div class="table-responsive">
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th>Name</th>
-                <th>Short code</th>
-                <th>Origin</th>
-                <th>Destination</th>
-                <th>Created At</th>
-                <th>Updated At</th>
-            </tr>
-            </thead>
-            <tbody>
+        <div class="card-body">
+            <h4 class="card-title">Routes</h4>
+            <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Short Code</th>
+                    <th>Origin</th>
+                    <th>Destination</th>
+                    <th>Created At</th>
+                    <th>Updated At</th>
+                    <th class="text-center">Action</th>
+                </tr>
+                </thead>
+                <tbody>
                 @foreach($routes as $route) 
                 <tr>
-                <td class="py-1">{{ $route->name }}</td>
-                <td>{{ $route->short_code }}</td>
-                <td>{{ $route->Origin }}</td>
-                <td>{{ $route->Destination }}</td>
-                <td>{{ $route->created_at }}</td>
-                <td>{{ $route->updated_at }}</td>
-            </tr>
+                    <td>{{ $route->name }}</td>
+                    <td>{{ $route->short_code }}</td>
+                    <td>{{ $route->origin_id }}</td>
+                    <td>{{ $route->destination_id }}</td>
+                    <td>{{ $route->created_at }}</td>
+                    <td>{{ $route->updated_at }}</td>
+                    <td class="text-center"> . </td>
+                </tr>
                 @endforeach
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+            </div>
         </div>
-    </div>
     </div>
 </div>
 @endsection
