e1e58c0ac9586e98bb51de65910af1e4f602c60c
emma ingurat <e.ingurat@bsmart-technology.com>
2022-07-27 11:16:53 +0300

    footer and header updates

---
+++ resources/views/layouts/core/base.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Kapella Bootstrap Admin Dashboard Template</title>
+  <!-- base:css -->
+  <link rel="stylesheet" href="../../vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="../../vendors/base/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="../../css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="../../images/favicon.png" />
+</head>
+
+<body>
+  <div class="container-scroller">
+        @include('layouts.core.partials.footer')
+    <div class="container-fluid page-body-wrapper">
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="row">
+
+          </div>
+        </div>
+         @include('layouts.core.partials.footer')
+      </div>
+    </div>
+  </div>
+  <script src="{{ asset('vendors/base/vendor.bundle.base.js') }}"></script>
+  <script src="{{ asset('js/template.js') }}"></script>
+</body>
+
+</html>
