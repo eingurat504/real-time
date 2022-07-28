09336db554bbe1028b6abd13b65ba6fee867b09a
emma ingurat <e.ingurat@bsmart-technology.com>
2022-07-28 18:08:35 +0300

    location types migrations

---
+++ app/Models/LocationType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LocationType extends Model
+{
+    use HasFactory;
+
+         /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'location_types';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+}
