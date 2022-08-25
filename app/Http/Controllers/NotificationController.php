f0ff03d770711d018fa8b8f8546e5e30e3381629
emma ingurat <e.ingurat@bsmart-technology.com>
2022-08-25 22:50:57 +0300

    send alert notification

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -9,7 +9,7 @@ use App\Notifications\ArrivalsNotification;
 
 class NotificationController extends Controller
 {
-    
+    // https://www.positronx.io/laravel-notification-example-create-notification-in-laravel/
     public function __construct()
     {
         $this->middleware('auth');
@@ -21,6 +21,23 @@ class NotificationController extends Controller
     }
     
     public function sendArrivalNotification() {
+        $userSchema = User::first();
+  
+        $offerData = [
+            'name' => 'BOGO',
+            'body' => 'You received an offer.',
+            'thanks' => 'Thank you',
+            'offerText' => 'Check out the offer',
+            'offerUrl' => url('/'),
+            'offer_id' => 007
+        ];
+  
+        Notification::send($userSchema, new OffersNotification($offerData));
+   
+        dd('Task completed!');
+    }
+
+    public function sendAlertNotification() {
         $userSchema = User::first();
   
         $offerData = [
