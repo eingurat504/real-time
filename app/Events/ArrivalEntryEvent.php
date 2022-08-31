f7f5ee67ea6d78c34db9d5a60968cb90432f587d
emma ingurat <e.ingurat@bsmart-technology.com>
2022-08-31 23:24:16 +0300

    entry event

---
+++ app/Events/ArrivalEntryEvent.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ArrivalEntryEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $data = ['asas'];
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('arrival-entry-channel');
+    }
+
+     /**
+     * The event's broadcast name.
+     *
+     * @return string
+     */
+    public function broadcastAs()
+    {
+        return 'EntryEvent';
+    }
+    /**
+     * The event's broadcast name.
+     *
+     * @return string
+     */
+    public function broadcastWith()
+    {
+        return ['title'=>'Notification message will go here'];
+    }
+}
