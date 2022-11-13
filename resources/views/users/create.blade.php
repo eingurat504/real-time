d23acb042fe59f7f12ebfd234c00b12dc105af26
emma ingurat <e.ingurat@bsmart-technology.com>
2022-11-13 19:07:47 +0300

    user form updates

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -46,21 +46,22 @@
                                     <span class="invalid-feedback" role="alert">{{ $message }}</span>
                                     @enderror
                                 </div>
+                                
+                                <div class="form-group">
+                                    <label for="username">Username: </label>
+                                    <input type="text" name="username" id="name" required
+                                        class="form-control @error('username') is-invalid @enderror"
+                                        value="{{ old('username') }}" placeholder="{{ __('username') }}"/>
+                                    @error('username')
+                                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                    @enderror
+                                </div>
                                 <div class="form-group">
                                     <label for="email">Email: </label>
                                     <input type="text" name="email" id="name" required
                                         class="form-control @error('email') is-invalid @enderror"
                                         value="{{ old('email') }}" placeholder="{{ __('email') }}"/>
                                     @error('email')
-                                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                                <div class="form-group">
-                                    <label for="phone_number">Phone number: </label>
-                                    <input type="text" name="phone_number" id="name" required
-                                        class="form-control @error('phone_number') is-invalid @enderror"
-                                        value="{{ old('phone_number') }}" placeholder="{{ __('phone_number') }}"/>
-                                    @error('phone_number')
                                     <span class="invalid-feedback" role="alert">{{ $message }}</span>
                                     @enderror
                                 </div>
@@ -71,6 +72,15 @@
                         <div class="card">
                             <div class="card-body">
                                 <h4 class="card-title">User Info</h4>
+                                <div class="form-group">
+                                    <label for="phone_number">Phone number: </label>
+                                    <input type="text" name="phone_number" id="name" required
+                                        class="form-control @error('phone_number') is-invalid @enderror"
+                                        value="{{ old('phone_number') }}" placeholder="{{ __('phone_number') }}"/>
+                                    @error('phone_number')
+                                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                    @enderror
+                                </div>
                                 <div class="form-group">
                                     <label>Status</label>
                                     <select type="text" name="status" id="status" required
